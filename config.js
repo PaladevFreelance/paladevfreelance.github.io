44d2a6e9e7bf1775759620082f2ac927f9dbad89
Thomas Palazzetti <84309068+PaladevFreelance@users.noreply.github.com>
2025-10-04 13:54:23 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,4 @@
+const SUPABASE_CONFIG = {
+    url: 'https://cvixliqjdxoeqbjokjjc.supabase.co',
+    key: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN2aXhsaXFqZHhvZXFiam9rampjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTk1MjI1NTIsImV4cCI6MjA3NTA5ODU1Mn0.LTuLpKXeOjptCNnv2BcV4GywG9MVtGHN5emu4ydhC3k'
+};
